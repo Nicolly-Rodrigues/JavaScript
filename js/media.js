0aed872f45d651a1bde26ebd5f1802e80cde33ed
Nicolly-Rodrigues <nicollyrodrigues.dev2024@gmail.com>
2025-03-13 15:53:59 -0300

    Projeto de soma e media

--- js/media.js
+++ js/media.js
@@ -1,8 +1,9 @@
-function (verificar){
+function verificar(){
     let n1= parseFloat(document.getElementById("num1").value);
     let n2= parseFloat(document.getElementById("num2").value);
     let n3= parseFloat(document.getElementById("num3").value);
     let n4= parseFloat(document.getElementById("num4").value);
-    verificar = n1+n2+n3+n4 /4;
-    document.getElementById("resultado").textContent="Resultado: " +verificar;
+
+    media = n1+n2+n3+n4 /4;
+    document.getElementById("resultado").textContent="Resultado: " +media;
 }
