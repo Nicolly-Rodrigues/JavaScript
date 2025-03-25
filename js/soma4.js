f4d4ab4833f130312b4c6aa49054a5fd596f0d54
Nicolly-Rodrigues <nicollyrodrigues.dev2024@gmail.com>
2025-03-25 12:23:44 -0300

    Exercicio 17 javaScript

--- js/soma4.js
+++ js/soma4.js
@@ -1,8 +1,5 @@
-function somar(){
-    let n1= parseFloat(document.getElementById("num1").value);
-    let n2= parseFloat(document.getElementById("num2").value);
-    let n3= parseFloat(document.getElementById("num2").value);
-    let n4= parseFloat(document.getElementById("num2").value);
-    let soma= n1+n2+n3+n4;
-    document.getElementById("resultado").textContent="Resultado: " +soma;
+function somar(a,b){
+   return a + b
 }
+let resultado = somar(5,10);
+console.log(resultado);
