95c800851723693016858e22345a6353d737ec52
Nicolly-Rodrigues <nicollyrodrigues.dev2024@gmail.com>
2025-04-01 10:27:18 -0300

    exercicios 28 javascript

--- js/impares.js
+++
@@ -1,9 +0,0 @@
-let resultadoDiv =document.getElementById("resultado")
-let numerosImpares ="";
-
-for(let i=0; i<=10; i++){
-    if (i %2 == 1){
-        numerosImpares += i + "<br>";
-    }
-}
-resultadoDiv.innerHTML = numerosImpares;
